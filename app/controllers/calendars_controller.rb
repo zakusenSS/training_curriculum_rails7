1e44a83914e5761137d12a73f35000546ed693a6
zakusenSS <cycpp866@yahoo.co.jp>
2025-04-02 21:12:43 +0900

    Revert "Merge branch 'master' into first_training"
    This reverts commit e687d38564ada4b24d0799b5ae9667b3f67720fc, reversing
    changes made to 6d59bb6dab197ccee6f324af5a16ff6aff203509.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -34,14 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday # wdayメソッドを用いて取得した数値
-
-      if wday_num >= 7 #「wday_numが7以上の場合」という条件式
-        wday_unm = wday_num -7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num] }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
